f7159459eeba4156ec6d2617f6f6bd27151d0f9a
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-08-22 16:17:10 +0100

    Create login route that utilises auth.js in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const revenueRoutes = require("./routes/revenue");
 const beehiivRoutes = require("./routes/beehiiv");
+const { authenticate } = require('./middleware/auth');
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ app.use(express.json());
 
 app.use("/api/revenue", revenueRoutes);
 app.use("/api/beehiiv", beehiivRoutes);
-
+app.post('/api/login', authenticate);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
